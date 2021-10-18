24c764e23cee57c375719d826d6cb018b69c57e1
jonah-cod <84820291+jonah-cod@users.noreply.github.com>
2021-10-18 11:47:51 +0300

    Update reverseNumbers.js
    "updated the loop to start from 100 downwards"

--- assignment2/reverseString&Numbers/reverseNumbers.js
+++ assignment2/reverseString&Numbers/reverseNumbers.js
@@ -1,13 +1,13 @@
 function reverseMof3() {
     const reversedMultiples = [];
-    for (let i = 1; i <= 100; i++) {
+    for (let i = 100; i > 1; i--) {
         if (i % 3 === 0) {
             reversedMultiples.push(i);
         }
 
     }
 
-    console.log(reversedMultiples.reverse());
+    console.log(reversedMultiples);
 }
 
 reverseMof3();
